445e83aa9aead9241c5c4fe9d7c5e72b70378c69
Junelle Puzon <nellepuzon@gmail.com>
2022-06-24 08:42:31 +0800

    validates title with no spaces

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,6 @@
 class Entry < ApplicationRecord
     belongs_to :user
-    validates :title, presence: :true
+    validates :title, presence: :true, format: {without: /\s/, message: "must contain no spaces"}
+    validates :content, presence: :true
+    validates_uniqueness_of :title, message: "already exists"
 end
